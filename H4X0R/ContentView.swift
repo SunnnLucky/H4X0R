96e4eae14ede599cd2d5324ae7d2c3cf6740d730
Lei Sun <455319106@qq.com>
2022-06-05 20:52:47 +0800

    creat list view

--- H4X0R/ContentView.swift
+++ H4X0R/ContentView.swift
@@ -7,16 +7,33 @@
 
 import SwiftUI
 
+struct Post : Identifiable{
+    let id: String
+    let title : String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Hola"),
+    Post(id: "3", title: "Hi"),
+    Post(id: "4", title: "Yo"),
+]
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-        
+        NavigationView {
+            List(posts) { post in
+                //Trailing Closures
+                Text(post.title)
+            }
+            .navigationTitle("H4X0R NEWS")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+//            .preferredColorScheme(.dark)
     }
 }
