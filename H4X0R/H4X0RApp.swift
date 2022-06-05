f98acf97fa7fb046c4dc48026897d8288d381ca0
Lei Sun <455319106@qq.com>
2022-06-05 20:03:26 +0800

    Initial Commit

---
+++ H4X0R/H4X0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RApp.swift
+//  H4X0R
+//
+//  Created by 孙磊 on 2022/6/5.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
